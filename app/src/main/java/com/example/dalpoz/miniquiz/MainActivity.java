78a14a5821875730bceabcf40617fe5835f92240
Stefano Dal Poz <dalpoz01@gmail.com>
2018-02-22 11:04:55 +0100

    Modifica a MainActivity

--- app/src/main/java/com/example/dalpoz/miniquiz/MainActivity.java
+++ app/src/main/java/com/example/dalpoz/miniquiz/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.dalpoz.miniquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button indietro=findViewById(R.id.button_Indietro);
+        Button avanti=findViewById(R.id.button_Avanti);
+        ScrollView sc=findViewById(R.id.scrollView2);
+        TextView tw=new TextView(null);
+        tw.setText("ciao");
+        sc.addView(tw);
     }
 }
