382e3356d1f8644a51ab2ff037cc20186f427b24
Stefano <dalpoz01@gmail.com>
2018-02-26 12:40:04 +0100

    Creazione classe Domande

---
+++ app/src/main/java/com/example/dalpoz/miniquiz/Domande.java
@@ -0,0 +1,28 @@
+package com.example.dalpoz.miniquiz;
+
+public class Domande {
+    public String Domande[]={
+        "Il Sole gira intorno alla Terra",
+        "Jury Chechi è un noto politico italiano",
+        "I Vegani non escludono dal loro stile di vita tutti i prodotti di origine animale. Per esempio mangiano il formaggio",
+        "Il sismografo è lo strumento che viene utilizzato per registrare i fenomeni sismici",
+        "Se mi trovo ad Ostia mi trovo a due passi da Firenze",
+        "Il Museo del Louvre si trova a Firenze"
+    };
+    public String Risposte[]={
+        "Falso",
+        "Falso",
+        "Falso",
+        "Vero",
+        "Falso",
+        "Falso"
+    };
+    public String getDomanda(int n){
+        String domanda=Domande[n];
+        return domanda;
+    }
+    public String getRisposta(int n){
+        String risposta=Risposte[n];
+        return risposta;
+    }
+}
--- app/src/main/java/com/example/dalpoz/miniquiz/MainActivity.java
+++ app/src/main/java/com/example/dalpoz/miniquiz/MainActivity.java
@@ -3,21 +3,22 @@ package com.example.dalpoz.miniquiz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button indietro,avanti;
+    ImageButton vero,falso;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button indietro=findViewById(R.id.button_Indietro);
-        Button avanti=findViewById(R.id.button_Avanti);
-        ScrollView sc=findViewById(R.id.scrollView2);
-        TextView tw=new TextView(null);
-        tw.setText("ciao");
-        sc.addView(tw);
+        indietro=findViewById(R.id.button_Indietro);
+        avanti=findViewById(R.id.button_Avanti);
+        vero=findViewById(R.id.imageButton_Vero);
+        falso=findViewById(R.id.imageButton_Falso);
+
     }
 }
