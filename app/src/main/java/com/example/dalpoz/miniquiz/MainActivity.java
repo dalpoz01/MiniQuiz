26f2082d24328beeeb3e756692999451feee5cc5
Stefanodp <dalpoz01@gmail.com>
2018-02-27 17:40:45 +0100

    Iniziata gestione domande e risposte, miglioramenti grafici

--- app/src/main/java/com/example/dalpoz/miniquiz/MainActivity.java
+++ app/src/main/java/com/example/dalpoz/miniquiz/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.dalpoz.miniquiz;
 import android.graphics.drawable.Icon;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ScrollView;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     Button indietro,avanti;
@@ -14,14 +18,17 @@ public class MainActivity extends AppCompatActivity {
     TextView nomeGiocatore,punteggio,domanda;
 
     private Domande domande=new Domande();
-    private String Risposta;
+    private String Risposta,Risposta_giusta;
     private int Punteggio=0;
     private int numero_domande=domande.Domande.length;
+    private int domande_eseguite=0;
+
+    Random r;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        r=new Random();
         indietro=(Button) findViewById(R.id.button_Indietro);
         avanti=(Button)findViewById(R.id.button_Avanti);
         vero=(ImageButton) findViewById(R.id.imageButton_Vero);
@@ -29,6 +36,49 @@ public class MainActivity extends AppCompatActivity {
         nomeGiocatore=(TextView) findViewById(R.id.textView_NomeGiocatore);
         punteggio=(TextView) findViewById(R.id.textView_Punteggio);
         domanda=(TextView) findViewById(R.id.textView_domanda);
-        domanda.setText(domande.getDomanda(1));
+        aggiornaDomanda(r.nextInt(numero_domande));
+
+        indietro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                aggiornaDomanda(r.nextInt(numero_domande));
+            }
+        });
+
+        avanti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                aggiornaDomanda(r.nextInt(numero_domande));
+            }
+        });
+
+        vero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(Risposta_giusta.equals("Vero")){
+                    Punteggio++;
+                    punteggio.setText("Punti: "+Punteggio);
+                    aggiornaDomanda(r.nextInt(numero_domande));
+                    Log.d("Pulsante","Schiacciato Vero");
+                }
+            }
+        });
+
+        falso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(Risposta_giusta.equals("Falso")){
+                    Punteggio++;
+                    punteggio.setText("Punti: "+Punteggio);
+                    aggiornaDomanda(r.nextInt(numero_domande));
+                    Log.d("Pulsante","Schiacciato Falso");
+                }
+            }
+        });
+
+    }
+    private void aggiornaDomanda(int num){
+        domanda.setText(domande.getDomanda(num));
+        Risposta_giusta = ""+domande.getRisposta(num);
     }
 }
